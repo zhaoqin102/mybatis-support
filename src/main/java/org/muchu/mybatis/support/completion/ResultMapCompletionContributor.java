2b6cf0ed84cdaa84520dd55b88d212bb67092446
haipeng.liang <haipeng.liang@tingjiandan.com>
2019-08-16 09:03:22 +0800

    add completion

--- src/main/java/org/muchu/mybatis/support/completion/ResultMapCompletionContributor.java
+++ src/main/java/org/muchu/mybatis/support/completion/ResultMapCompletionContributor.java
@@ -8,9 +8,10 @@ import com.intellij.codeInsight.lookup.LookupElementBuilder;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.xml.XmlAttribute;
-import com.intellij.ui.mac.MacPathChooserDialog;
 import com.intellij.util.xml.DomFileElement;
+import com.intellij.util.xml.DomManager;
 import com.intellij.util.xml.DomService;
+import com.intellij.util.xml.GenericAttributeValue;
 import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.muchu.mybatis.support.model.Mapper;
@@ -30,6 +31,7 @@ public class ResultMapCompletionContributor extends CompletionContributor {
         PsiElement parent = psiElement.getParent().getParent();
         if (!(parent instanceof XmlAttribute)) return;
         XmlAttribute xmlAttribute = (XmlAttribute) parent;
+        GenericAttributeValue domElement = DomManager.getDomManager(xmlAttribute.getProject()).getDomElement(xmlAttribute);
         if (!StringUtils.equals(xmlAttribute.getName(), myAttributeName)) return;
         result.restartCompletionWhenNothingMatches();
         List<DomFileElement<Mapper>> fileElements = DomService.getInstance().getFileElements(Mapper.class, psiElement.getProject(),
@@ -37,7 +39,7 @@ public class ResultMapCompletionContributor extends CompletionContributor {
         for (DomFileElement<Mapper> fileElement : fileElements) {
             List<ResultMap> resultMaps = fileElement.getRootElement().getResultMaps();
             for (ResultMap resultMap : resultMaps) {
-                result.addElement(LookupElementBuilder.createWithSmartPointer(fileElement.getRootElement().getNamespace() + "." + resultMap.getId(), resultMap.getXmlTag()));
+                result.addElement(LookupElementBuilder.create(resultMap.getXmlTag(), fileElement.getRootElement().getNamespace() + "." + resultMap.getId()));
             }
         }
         super.fillCompletionVariants(parameters, result);
